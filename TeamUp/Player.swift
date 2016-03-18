3d39c52470a868f26c237e78dc10b10037ded96a
johansnels <johansnels@hotmail.com>
2016-03-18 17:18:44 +0100

    Player class almost finished

---
+++ TeamUp/Player.swift
@@ -0,0 +1,30 @@
+//
+//  Player.swift
+//  TeamUp
+//
+//  Created by johan on 18/03/16.
+//  Copyright © 2016 johan. All rights reserved.
+//
+
+import Foundation
+
+class Player {
+
+    //MARK: - Attributes
+    var fName : String
+    var lName : String?
+    var birthdate : NSDate?
+    var position : String?
+    var profilePic : String?
+    var gamesPlayed : Int?
+    
+    
+    //MARK: - Constructors
+    init(fName:String){
+        self.fName = fName
+    }
+    
+    
+    //MARK: - Custom Functions
+
+}
